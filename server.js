6d843ec383eb6a3a2329449c3f5bac6e6adb7517
maedashogun <maedahikaro@gmail.com>
2019-12-06 14:03:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({message:'Olá'});
 })
 
+app.get('/ola', (req, res)=>{
+    res.status(200).send({message:'Olá, nova rota'});
+})
+
 app.listen(3000, ()=>{
     console.log('Api rodando')
 })
